33a3cd70384b207e2965efdcf2919fb73d78acc0
Luke Mullikin <lukemull11@gmail.com>
2025-01-09 20:59:36 -0500

    sofle: add my confs to existing sofle/rev1:vial

---
+++ keyboards/sofle/rev1/keymaps/vial/defs.h
@@ -0,0 +1,17 @@
+/**
+ * Extra definitions for use between Sofle Vial source files
+ *
+ * 2025 Luke Mullikin
+ */
+
+#pragma once
+
+enum SOFLE_LAYERS
+{
+  _DEFAULT = 0,
+  _COLEMAK_DH = 0,
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _NAV,
+};
--- keyboards/sofle/rev1/keymaps/vial/oled.c
+++ keyboards/sofle/rev1/keymaps/vial/oled.c
@@ -1,21 +1,24 @@
  /* Copyright 2020 Josef Adamcik
   * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
-  * 
-  * This program is free software: you can redistribute it and/or modify 
-  * it under the terms of the GNU General Public License as published by 
-  * the Free Software Foundation, either version 2 of the License, or 
-  * (at your option) any later version. 
-  * 
-  * This program is distributed in the hope that it will be useful, 
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-  * GNU General Public License for more details. 
-  * 
-  * You should have received a copy of the GNU General Public License 
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-  */ 
+  * Personalized by/for Luke Mullikin 2025
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
 
 //Sets up what the OLED screens display.
+
+#include "defs.h"
 
 #ifdef OLED_ENABLE
 
@@ -34,33 +37,35 @@ static void print_status_narrow(void) {
     oled_write_P(PSTR("\n\n"), false);
 
     switch (get_highest_layer(layer_state)) {
-        case 0:
-            oled_write_ln_P(PSTR("Qwrt"), false);
+        case _COLEMAK_DH:
+            oled_write_ln_P(PSTR("CmkDH"), false);
             break;
-        case 1:
-            oled_write_ln_P(PSTR("Clmk"), false);
+        case _QWERTY:
+            oled_write_ln_P(PSTR("Qwrt"), false);
             break;
         default:
             oled_write_P(PSTR("Mod\n"), false);
             break;
     }
     oled_write_P(PSTR("\n\n"), false);
+
     // Print current layer
     oled_write_ln_P(PSTR("LAYER"), false);
     switch (get_highest_layer(layer_state)) {
-        case 0:
-        case 1:
+        case _COLEMAK_DH:
+        case _QWERTY:
             oled_write_P(PSTR("Base\n"), false);
             break;
-        case 2:
-            oled_write_P(PSTR("Raise"), false);
-            break;
-        case 3:
+        case _LOWER:
             oled_write_P(PSTR("Lower"), false);
+            break;
+        case _RAISE:
+            oled_write_P(PSTR("Raise"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undef"), false);
     }
+
     oled_write_P(PSTR("\n\n"), false);
     led_t led_usb_state = host_keyboard_led_state();
     oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
