e7500be8dc0ea4d6a60b2dd39068fec3083c038b
Luke Mullikin <lukemull11@gmail.com>
2025-11-07 09:04:38 -0500

    sofle: space on cmk base layer

--- keyboards/sofle/rev1/keymaps/vial/keymap.c
+++ keyboards/sofle/rev1/keymaps/vial/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_QUOT,
   KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_H,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_BSPC,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MUTE, TO(_QWERTY),KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                  KC_LGUI, KC_LCTL, KC_LALT, MO(3),  MO(2),     KC_ENT,  KC_TRNS, MO(4), KC_RCTL, KC_RALT
+                  KC_LGUI, KC_LCTL, KC_LALT, MO(3),  MO(2),     KC_ENT,  KC_SPACE, MO(4), KC_RCTL, KC_RALT
 ),
 /*
  * QWERTY
